ea14803322139e74789d0a76b45bdabcbe161a8d
Sergio Leech <mainactivity2020@gmail.com>
2020-11-11 12:58:40 +0200

    Update Constants.kt

--- app/src/main/java/com/example/inchat/notification/Constants.kt
+++ app/src/main/java/com/example/inchat/notification/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
         const val SERVER_KEY =
-            "AAAAOxfbiqw:APA91bHmOUS9khWIcjv75--t69di_AbxsZRK5QjNsx8NQUlgOYK8RO-M24ckDcvyXJUT2JTNYXlel50S22j-mlOly3KLrlQfAspsIOiERjJgq2VNM3B44SaocKc2yra2lsaT91g7DwLG"
+            "set firebase message key"
         const val CONTENT_TYPE = "application/json"
     }
 }
